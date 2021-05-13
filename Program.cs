f45105b184a78be7bff74807428b211d4f043e92
G-75 <80418719+G-75@users.noreply.github.com>
2021-05-13 21:57:35 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> kelimeler = new MyDictionary<string>();
+            kelimeler.Add("bilek");
+            kelimeler.Add("bıçak");
+            kelimeler.Add("bileklik");
+            kelimeler.Add("bahçe");
+
+            foreach (var item in kelimeler.Items)
+            {
+                Console.WriteLine(item);
+
+
+            }
+        }
+    }
+}
+
+
